a01b81ffa87946bd5516bd2c6539696de41c878b
Muhammad Rezaul Karim <33606386+muhammadrezaulkarim@users.noreply.github.com>
2018-06-10 18:31:55 -0600

    specific account
    The postgres user account is the only user account and is set up with no password and with peer authentication from the postgres unix user account, meaning that you can connect to the postgres PostgreSQL account if you run the client program from the postgres unix account.
    
    This means that you start with:
    
    sudo -u postgres psql
    from whence you can create users, databases, etc. sudo -u postgres createdb etc also work if you prefer the shell wrapper commands to CREATE USER, CREATE DATABASE etc SQL commands.

--- postgres-entrypoint.sh
+++ postgres-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-psql -d postgres -U postgres
+sudo -u postgres psql -d postgres -U postgres
 create extension bottledwater;
 create table test (id serial primary key, value text);
 insert into test (value) values('hello world!');
