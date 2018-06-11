71cb4863d2423832096ae2c12f90bc87e94207e1
Muhammad Rezaul Karim <33606386+muhammadrezaulkarim@users.noreply.github.com>
2018-06-10 21:33:41 -0600

    change the commands

--- postgres-entrypoint.sh
+++ postgres-entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-sudo -u postgres psql -d postgres -U postgres -c "create extension bottledwater;"
-sudo -u postgres psql -d postgres -U postgres -c "create table test (id serial primary key, value text);"
-sudo -u postgres psql -d postgres -U postgres -c "insert into test (value) values('hello world!');"
+# only 'postgres' linux user can run sql commands in POSTGRESQL. 
+# We have set 'postgres' user as super user by setting POSTGRES_USER variable in the docker file. 
+# Superuser password set in the POSTGRES_PASSWORD environment variable
+
+psql -d postgres -U postgres -c "create extension bottledwater;"
+psql -d postgres -U postgres -c "create table test (id serial primary key, value text);"
+psql -d postgres -U postgres -c "insert into test (value) values('hello world!');"
+
