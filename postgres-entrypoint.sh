d764148f5492f6e18e4cf4a66d6fd32807972e95
Muhammad Rezaul Karim <33606386+muhammadrezaulkarim@users.noreply.github.com>
2018-06-10 20:35:56 -0600

    Update postgres-entrypoint.sh

--- postgres-entrypoint.sh
+++ postgres-entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-su - postgres
-psql -d postgres -U postgres
-create extension bottledwater;
-create table test (id serial primary key, value text);
-insert into test (value) values('hello world!');
+sudo -u postgres psql -d postgres -U postgres -c "create extension bottledwater;"
+sudo -u postgres psql -d postgres -U postgres -c "create table test (id serial primary key, value text);"
+sudo -u postgres psql -d postgres -U postgres -c "insert into test (value) values('hello world!');"
