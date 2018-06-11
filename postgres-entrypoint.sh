d1533ee1822815dc21e7d5bed2d38c924a128b75
Muhammad Rezaul Karim <33606386+muhammadrezaulkarim@users.noreply.github.com>
2018-06-10 18:57:19 -0600

    connect/login as root user for postgres

--- postgres-entrypoint.sh
+++ postgres-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-adduser postgres sudo
-sudo -u postgres psql -d postgres -U postgres
+su - postgres
+psql -d postgres -U postgres
 create extension bottledwater;
 create table test (id serial primary key, value text);
 insert into test (value) values('hello world!');
